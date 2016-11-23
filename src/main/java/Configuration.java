4d34f9665602090008807ece70053f52ca51db4d
Hubert Slojewski <hslojewski@grupainteger.pl>
2016-11-23 10:30:40 +0100

    Add default configuration values

--- src/main/java/Configuration.java
+++ src/main/java/Configuration.java
@@ -2,8 +2,10 @@ import org.aeonbits.owner.Config;
 
 public interface Configuration extends Config {
     @Key("aws.credentials.accessKeyId")
+    @DefaultValue("provide-configuration-file")
     String getAccessKeyId();
 
     @Key("aws.credentials.secretAccessKey")
+    @DefaultValue("provide-configuration-file")
     String getSecretAccessKey();
 }
